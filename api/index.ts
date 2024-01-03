ce3ff3bcf10536f70a764bc27cc5275bba423f2d
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-01-03 10:53:51 +0100

    Added vercel cli, change target to ES6

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function (request: VercelRequest, response: VercelResponse) {
     const { name = 'World' } = request.query;
-    response.status(200).send(`Hello ${name}!`);
+    // response.status(200).send(`Hello Mister ${name}!`);
+    response.status(200).json({ name: `Hello Mister ${name}!` });
 }
