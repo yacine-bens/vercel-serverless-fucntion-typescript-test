a524b6c91a6c98f5f983da1750825163e56c6516
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-01-03 10:54:48 +0100

    Change response text

--- api/index.ts
+++ api/index.ts
@@ -3,5 +3,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function (request: VercelRequest, response: VercelResponse) {
     const { name = 'World' } = request.query;
     // response.status(200).send(`Hello Mister ${name}!`);
-    response.status(200).json({ name: `Hello Mister ${name}!` });
+    response.status(200).json({ name: `Hello ${name}!` });
 }
